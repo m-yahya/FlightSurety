a06f1b568a187a5c6c7cd317281754a3e21b3cd4
m-yahya <yahya_gis@hotmail.com>
2019-12-04 15:02:13 +0100

    Fix first airline registration

--- config/testConfig.js
+++ config/testConfig.js
@@ -21,7 +21,7 @@ var Config = async function(accounts) {
 
 
     let owner = accounts[0];
-    let firstAirline = accounts[1];
+    let firstAirline = accounts[0];
 
     let flightSuretyData = await FlightSuretyData.new();
     let flightSuretyApp = await FlightSuretyApp.new(flightSuretyData.address);
