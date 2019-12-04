4362775c5082cfa83ef3eb1ccb7bb2ebce24c1c4
m-yahya <yahya_gis@hotmail.com>
2019-12-04 14:26:56 +0100

    Fix nonce issue

--- truffle.js
+++ truffle.js
@@ -1,11 +1,16 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker")
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        var wallet = new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        var nonceTracker = new NonceTrackerSubprovider()
+        wallet.engine._providers.unshift(nonceTracker)
+        nonceTracker.setEngine(wallet.engine)
+        return wallet 
       },
       network_id: '*'
     }
