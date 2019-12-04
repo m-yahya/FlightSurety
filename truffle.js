e9bb11d15949be026b2f05cad36c86a46634e81c
m-yahya <yahya_gis@hotmail.com>
2019-12-04 14:32:46 +0100

    Updat config to fix nonce issue

--- truffle.js
+++ truffle.js
@@ -4,15 +4,21 @@ var NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-t
 
 module.exports = {
   networks: {
-    development: {
+    
+      development: {
+        host: "127.0.0.1",
+        port: 8545,
+        network_id: "*" // Match any network id
+      },
+      rinkeby: {
       provider: function() {
-        var wallet = new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        var wallet = new HDWalletProvider(mnemonic, ENDPOINT);
         var nonceTracker = new NonceTrackerSubprovider()
         wallet.engine._providers.unshift(nonceTracker)
         nonceTracker.setEngine(wallet.engine)
         return wallet 
       },
-      network_id: '*'
+      network_id: '4'
     }
   },
   compilers: {
